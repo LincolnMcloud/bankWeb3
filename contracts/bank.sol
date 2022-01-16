bbd172ae168c4f286c1104edc03f76d9f33d1ee2
LincolnMcloud <lamcloudjr@gmail.com>
2022-01-16 02:27:03 -0500

    second commit

--- contracts/bank.sol
+++ contracts/bank.sol
@@ -5,11 +5,12 @@ contract Bank{
     struct Account{
         address owner;
         uint balance;
-        uint depositTime;
+        uint timeStamp;
     }
 
     mapping(address => Account) public accounts;
     uint public interestRate = 3;
+    uint public totalether = 0;
 
 
     function creatAccount() public{
@@ -17,13 +18,37 @@ contract Bank{
         accounts[msg.sender] = newAccount;
     }
 
-    function deposit() public payable{
+    function deposit(address _account) public payable{
         require(msg.value > 0, "Insuffienct amount of funds");
-        accounts[msg.sender].balance = accounts[msg.sender].balance + msg.value;
+        accounts[_account].balance = accounts[_account].balance + msg.value;
+        accounts[_account].timeStamp = block.timestamp;
+        totalether = totalether + msg.value;
         // add interest
     }
 
-    // function interestCal() internal{
+    function getBalance(address _account) public returns(uint){
+        require(accounts[_account].owner == _account, "account doesn't exist");
+        Account memory accountInfo = accounts[_account];
+        uint timePassed = block.timestamp - accounts[_account].timeStamp;
+        //(Simple Interest × 100)/(Principal × Time)
+        uint newBalance = accountInfo.balance + uint((timePassed * interestRate * accountInfo.balance)/ (100 * 365 * 24 * 60 * 60));
+        return newBalance;
 
-    // }
+    }
+
+    function withdrawAll() public{
+        require(accounts[msg.sender].owner == msg.sender, "account doesn't exist");
+        address payable reciever = payable(msg.sender);
+        uint amount = getBalance(reciever);
+
+        require(totalether > amount, "There isn't enough funds in the contract");
+        reciever.transfer(amount);
+        accounts[msg.sender].timeStamp = block.timestamp;
+
+        totalether = totalether - amount;
+    }
+
+    fallback() external payable{
+        totalether = totalether + msg.value;
+    }
 }
